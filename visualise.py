c51423453e254e9e73141e071b744be188b1b06b
Angus Hollands <goosey15@gmail.com>
2016-09-27 15:46:44 +0100

    Fix graph vis

--- visualise.py
+++ visualise.py
@@ -2,17 +2,27 @@ from networkx import Graph, draw_networkx
 from matplotlib import pyplot as plt
 
 
+def repr_action(action, i):
+    return "{!r}_{}".format(action, i)
+
+
 def visualise_plan(plan, filename):
     graph = Graph()
-    for step in plan.plan_steps:
-        graph.add_node(step.action)
+
+    for i, step in enumerate(plan.plan_steps):
+        name = repr_action(step, i)
+        graph.add_node(name)
 
     if len(plan.plan_steps) > 1:
         it = iter(plan.plan_steps)
         next(it)
 
+        i = 0
         for step, next_step in zip(plan.plan_steps, it):
-            graph.add_edge(step.action, next_step.action)
+            name = repr_action(step, i)
+            next_name = repr_action(next_step, i + 1)
+            i += 1
+            graph.add_edge(name, next_name)
 
     plt.axis('off')
     draw_networkx(graph)
